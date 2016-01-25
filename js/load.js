621634e11d326e400d973201c181e7ca907a6c02
Kshitij <kshitijtwr@gmail.com>
2016-01-25 16:16:48 +0530

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,1 +1,5 @@
-$( "html" ).removeClass( "loading" );
+// IE10+
+document.getElementsByTagName( "html" )[0].classList.remove( "loader" );
+
+// All browsers
+document.getElementsByTagName( "html" )[0].className.replace( /loader/, "" );
