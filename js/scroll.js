3233cc01a7dda008f46e45f5e6159c776898769a
Kshitij <kshitijtwr@gmail.com>
2016-01-25 15:21:42 +0530

    Create scroll.js

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+/* 
+ * Shows the fixed header when, scrolling down, the static header disappears
+ */
+// Clone the header html into the hidden div
+$("header").clone().appendTo("#fading-header");
+
+// Get the header height
+var headerHeight = $("header").outerHeight();
+
+// Scrolling down it slides down/up the fixed header
+$(window).on('scroll', function() {
+  if ($(this).scrollTop() > headerHeight) {
+    $("#fading-header").slideDown(); // or fade in with .fadeIn()
+  } else {
+    $("#fading-header").slideUp(); // or fade out with .fadeIn
+  }
+});
